78bc9c79780e9c1630003ab16f9b4e12733a2342
miekxd <mike.nguyen0105@gmail.com>
2025-06-28 14:31:27 +0700

    added permission for FE

--- main.py
+++ main.py
@@ -97,7 +97,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:5173", 
         "http://localhost:3003",
-        "https://growthos-one.vercel.app" 
+        "https://growthos-one.vercel.app",
+        "https://growthy-xlef.vercel.app" 
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly include OPTIONS
