669e42203f929008b446c1e70b624b058d63a580
miekxd <mike.nguyen0105@gmail.com>
2025-06-30 15:33:04 +0700

    perms for new deployment

--- main.py
+++ main.py
@@ -101,6 +101,7 @@ app.add_middleware(
         "https://frontend-ruby-seven-45.vercel.app",
         "https://frontend-bckh94c82-growthos-projects.vercel.app",
         "https://frontend-rmctsqv7b-growthos-projects.vercel.app",
+        "https://frontend-o4mkr2mon-growthos-projects.vercel.app",    
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly include OPTIONS
