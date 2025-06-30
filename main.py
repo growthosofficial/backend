042b87bc762683b0084adba81a7ec6a2ca5ce7e4
miekxd <mike.nguyen0105@gmail.com>
2025-06-30 15:46:19 +0700

    origin for new deployment

--- main.py
+++ main.py
@@ -101,7 +101,8 @@ app.add_middleware(
         "https://frontend-ruby-seven-45.vercel.app",
         "https://frontend-bckh94c82-growthos-projects.vercel.app",
         "https://frontend-rmctsqv7b-growthos-projects.vercel.app",
-        "https://frontend-o4mkr2mon-growthos-projects.vercel.app",    
+        "https://frontend-o4mkr2mon-growthos-projects.vercel.app",
+        "https://frontend-nhknneucn-growthos-projects.vercel.app",    
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly include OPTIONS
